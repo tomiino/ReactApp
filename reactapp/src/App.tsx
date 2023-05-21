b1b54fb84f3b67fbcb64b62d9d4c5fd31db9d3de
Tomas Hruby <hrubytom@outlook.com>
2023-05-21 16:15:18 +0200

    app created

---
+++ reactapp/src/App.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+/* // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAg3NfxwPSFKHnJhQ1laxO3DOAemy0U9RM",
+  authDomain: "reactapp0411.firebaseapp.com",
+  projectId: "reactapp0411",
+  storageBucket: "reactapp0411.appspot.com",
+  messagingSenderId: "119452348328",
+  appId: "1:119452348328:web:c900e2c025556bd74cf79d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig); */
